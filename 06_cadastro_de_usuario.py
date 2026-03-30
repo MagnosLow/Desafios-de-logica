137c80d114f01f658c9c098a31148bd0b4910b10
Artur Magno <arturmagnonew@gmail.com>
2026-03-30 19:55:52 -0300

    Update 06_cadastro_de_usuario.py

--- 06_cadastro_de_usuario.py
+++ 06_cadastro_de_usuario.py
@@ -2,3 +2,17 @@
 
 O sistema deve exibir as informações do usuário em um texto amigável.
 '''
+nome = input("insira seu nome:" )
+sexo = input("insira seu sexo: " )
+endereço = input("insira seu endereço:" )
+cidade = input("insira seu endereço:" )
+estado = input("insira seu estado:" )
+cep = input("insira seu cep: ")
+telefone = input("insira seu telefone:" )
+data = input("insira sua data de nascimento: ")
+rg = input("insira seu rg:" )
+pai = input("insira o nome do seu pai:" )
+mãe = input("insira o nome da sua mãe:" )
+escola = input("insira o seu grau de escolaridade:" )
+
+
