b6a9b754cf22e00f6c2d9b3263864b4c7f99f3bd
Artur Magno <arturmagnonew@gmail.com>
2026-03-31 20:56:23 -0300

    Update 07_troco_da_lanchonete.py

--- 07_troco_da_lanchonete.py
+++ 07_troco_da_lanchonete.py
@@ -1,3 +1,7 @@
 '''Daniel foi na padaria biju comprar um lanche. 
 Desenvolva um sistema que receba o valor do lanche e o valor que Daniel pagou e por fim calcule o troco que Daniel deverá receber da padaria.
 '''
+valor = float(input("valor do lanche: "))
+lanche = float(input("qual o seu valor: "))
+print(lanche - valor)
+print(f"aqui está seu troco: {lanche - valor}")
